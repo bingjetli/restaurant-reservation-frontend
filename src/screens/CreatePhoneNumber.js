6dba023a1168c415ca613079caca1dc7720431f5
bingjetli <bingjetli88@gmail.com>
2022-09-13 09:02:59 -0230

    reverting

--- src/screens/CreatePhoneNumber.js
+++ src/screens/CreatePhoneNumber.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { Image, KeyboardAvoidingView, Text, TextInput, TouchableHighlight, View } from 'react-native';
+import { Image, Text, TextInput, TouchableHighlight, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ArrowBackIosIcon from '../../assets/icons/arrow_back_ios.png';
 import { appColors } from '../common';
@@ -78,75 +78,73 @@ export default function({route, navigation}){
     }
 
     return (<SafeAreaView style={[global_styles.fullView, setup_styles.mainView]}>
-        <KeyboardAvoidingView behavior='position' contentContainerStyle={global_styles.fullView}>
-            <View style={global_styles.headerView}>
-                <TouchableHighlight 
-                    style={global_styles.headerBackButton} 
-                    activeOpacity={0.6}
-                    underlayColor={appColors.iosSystemGray5.light}
-                    onPress={returnToHomeScreen} >
-                    <>
-                        <Image style={global_styles.headerBackButtonIcon} source={ArrowBackIosIcon} />
-                        <Text style={global_styles.headerBackButtonText} >Cancel</Text>
-                    </>
-                </TouchableHighlight>
-                <Text style={global_styles.headerText}>Add Reservation</Text>
-                <View style={{flex:1}}></View>
+        <View style={global_styles.headerView}>
+            <TouchableHighlight 
+                style={global_styles.headerBackButton} 
+                activeOpacity={0.6}
+                underlayColor={appColors.iosSystemGray5.light}
+                onPress={returnToHomeScreen} >
+                <>
+                    <Image style={global_styles.headerBackButtonIcon} source={ArrowBackIosIcon} />
+                    <Text style={global_styles.headerBackButtonText} >Cancel</Text>
+                </>
+            </TouchableHighlight>
+            <Text style={global_styles.headerText}>Add Reservation</Text>
+            <View style={{flex:1}}></View>
+        </View>
+        <View style={[global_styles.fullCenteringView, setup_styles.bodyView]}>
+            <Text style={[global_styles.bodyHeading, setup_styles.bodyHeading]} >Phone Number</Text>
+            <Text style={[global_styles.bodyText, setup_styles.bodyText]} >Almost done! Please enter a contact number for this Reservation.</Text>
+            <View style={phone_number_styles.falseTextBox}>
+                <Text style={phone_number_styles.falseTextBoxText} >(</Text>
+                <TextInput 
+                    style={[phone_number_styles.invisibleTextBox, {minWidth:40}]}
+                    value={s_area_code}
+                    maxLength={3}
+                    keyboardType='number-pad'
+                    onChangeText={areaCodeTextChange}
+                    onKeyPress={areaCodeKeyPress}
+                    onPressIn={() => setAreaCodeState('')}
+                    ref={r_area_code_textbox} />
+                <Text style={phone_number_styles.falseTextBoxText} >)</Text>
+                <TextInput 
+                    style={[phone_number_styles.invisibleTextBox, {minWidth:40}]}
+                    value={s_prefix}
+                    maxLength={3}
+                    keyboardType='number-pad'
+                    autoFocus={true}
+                    onChangeText={prefixTextChange}
+                    onKeyPress={prefixKeyPress}
+                    onPressIn={() => setPrefixState('')}
+                    ref={r_prefix_textbox} />
+                <Text style={phone_number_styles.falseTextBoxText} >&ndash;</Text>
+                <TextInput 
+                    style={[phone_number_styles.invisibleTextBox, {minWidth:50}]}
+                    value={s_line_number}
+                    maxLength={4}
+                    keyboardType='number-pad'
+                    onChangeText={lineNumberTextChange}
+                    onKeyPress={lineNumberKeyPress}
+                    onPressIn={() => setLineNumberState('')}
+                    ref={r_line_number_textbox} />
             </View>
-            <View style={[global_styles.fullCenteringView, setup_styles.bodyView]}>
-                <Text style={[global_styles.bodyHeading, setup_styles.bodyHeading]} >Phone Number</Text>
-                <Text style={[global_styles.bodyText, setup_styles.bodyText]} >Almost done! Please enter a contact number for this Reservation.</Text>
-                <View style={phone_number_styles.falseTextBox}>
-                    <Text style={phone_number_styles.falseTextBoxText} >(</Text>
-                    <TextInput 
-                        style={[phone_number_styles.invisibleTextBox, {minWidth:40}]}
-                        value={s_area_code}
-                        maxLength={3}
-                        keyboardType='number-pad'
-                        onChangeText={areaCodeTextChange}
-                        onKeyPress={areaCodeKeyPress}
-                        onPressIn={() => setAreaCodeState('')}
-                        ref={r_area_code_textbox} />
-                    <Text style={phone_number_styles.falseTextBoxText} >)</Text>
-                    <TextInput 
-                        style={[phone_number_styles.invisibleTextBox, {minWidth:40}]}
-                        value={s_prefix}
-                        maxLength={3}
-                        keyboardType='number-pad'
-                        autoFocus={true}
-                        onChangeText={prefixTextChange}
-                        onKeyPress={prefixKeyPress}
-                        onPressIn={() => setPrefixState('')}
-                        ref={r_prefix_textbox} />
-                    <Text style={phone_number_styles.falseTextBoxText} >&ndash;</Text>
-                    <TextInput 
-                        style={[phone_number_styles.invisibleTextBox, {minWidth:50}]}
-                        value={s_line_number}
-                        maxLength={4}
-                        keyboardType='number-pad'
-                        onChangeText={lineNumberTextChange}
-                        onKeyPress={lineNumberKeyPress}
-                        onPressIn={() => setLineNumberState('')}
-                        ref={r_line_number_textbox} />
-                </View>
-            </View>
-            <View style={setup_styles.footerView}>
-                <TouchableHighlight 
-                    style={s_area_code.length === 3 && s_prefix.length === 3 && s_line_number.length === 4 ? global_styles.primaryButton : global_styles.primaryButtonDisabled} 
-                    activeOpacity={0.6}
-                    underlayColor={appColors.mainComplementary1}
-                    onPress={goToNextScreen} 
-                    disabled={!(s_area_code.length === 3 && s_prefix.length === 3 && s_line_number.length === 4)}>
-                    <Text style={global_styles.primaryButtonText}>Continue</Text>
-                </TouchableHighlight>
-                <TouchableHighlight 
-                    style={global_styles.secondaryButton} 
-                    activeOpacity={0.6}
-                    underlayColor={appColors.iosSystemGray5.light}
-                    onPress={goToPreviousScreen}>
-                    <Text style={global_styles.secondaryButtonText}>Go Back</Text>
-                </TouchableHighlight>
-            </View>
-        </KeyboardAvoidingView>
+        </View>
+        <View style={setup_styles.footerView}>
+            <TouchableHighlight 
+                style={s_area_code.length === 3 && s_prefix.length === 3 && s_line_number.length === 4 ? global_styles.primaryButton : global_styles.primaryButtonDisabled} 
+                activeOpacity={0.6}
+                underlayColor={appColors.mainComplementary1}
+                onPress={goToNextScreen} 
+                disabled={!(s_area_code.length === 3 && s_prefix.length === 3 && s_line_number.length === 4)}>
+                <Text style={global_styles.primaryButtonText}>Continue</Text>
+            </TouchableHighlight>
+            <TouchableHighlight 
+                style={global_styles.secondaryButton} 
+                activeOpacity={0.6}
+                underlayColor={appColors.iosSystemGray5.light}
+                onPress={goToPreviousScreen}>
+                <Text style={global_styles.secondaryButtonText}>Go Back</Text>
+            </TouchableHighlight>
+        </View>
     </SafeAreaView>);
 }
