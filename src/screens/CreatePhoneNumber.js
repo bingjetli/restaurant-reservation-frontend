1fbf59f18fdef3a9b95ccec6d05e285029da18f3
bingjetli <bingjetli88@gmail.com>
2022-09-13 09:01:28 -0230

    position

--- src/screens/CreatePhoneNumber.js
+++ src/screens/CreatePhoneNumber.js
@@ -78,7 +78,7 @@ export default function({route, navigation}){
     }
 
     return (<SafeAreaView style={[global_styles.fullView, setup_styles.mainView]}>
-        <KeyboardAvoidingView behavior='height'>
+        <KeyboardAvoidingView behavior='position'>
             <View style={global_styles.headerView}>
                 <TouchableHighlight 
                     style={global_styles.headerBackButton} 
